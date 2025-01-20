14bda7c4bf5f84576ea060a21a3425aed26489c7
winduu14 <officialvivo74@gmail.com>
2025-01-20 20:42:30 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Toggle menu visibility on mobile
+document.getElementById('menu-button').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
